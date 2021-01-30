d83698f4af654a790d37ec13fa77a945862d953a
DilipSinghDangwal <62637748+d-rat@users.noreply.github.com>
2021-01-30 13:10:23 +0530

    removed console.logs

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const usersRoute = require("./routes/users");
 
 app.use(bodyParser.json());
 app.use("/users", usersRoute);
-mongoose.connect(
-  process.env.DATABASE_CONNECTION,
-  { useNewUrlParser: true },
-  () => {
-    console.log("connected");
-  }
-);
+mongoose.connect(process.env.DATABASE_CONNECTION, { useNewUrlParser: true });
 
 app.listen(3000);
